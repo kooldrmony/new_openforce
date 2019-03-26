456a6916bee11b569ae070392f39db881afbfd09
kooldrmony <derrickreyes.23@gmail.com>
2019-03-25 19:50:41 -0700

    Updates

---
+++ precache-manifest.2960b64e4efa0ef10c6ef0457450c0e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a3645574113bedda3a7",
+    "url": "/new_openforce/static/js/runtime~main.7dcdfc3e.js"
+  },
+  {
+    "revision": "c7ff2622879205e12f82",
+    "url": "/new_openforce/static/js/main.a103a4b0.chunk.js"
+  },
+  {
+    "revision": "3dec49bd995c4a30a000",
+    "url": "/new_openforce/static/js/2.2a659faa.chunk.js"
+  },
+  {
+    "revision": "c7ff2622879205e12f82",
+    "url": "/new_openforce/static/css/main.13d616b3.chunk.css"
+  },
+  {
+    "revision": "60af1f688a45365f9e070bf1abf8a2ac",
+    "url": "/new_openforce/index.html"
+  }
+];
